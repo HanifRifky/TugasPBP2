5b385290601246243f8b5ef7a98cc1b79f34bfa0
HanifRifky <nurrifkyhanif@gmail.com>
2023-09-11 08:14:43 +0700

    Revisi views.py

--- main/views.py
+++ main/views.py
@@ -6,7 +6,8 @@ def show_main(request):
     context = {
         'name': 'Muhamad Hanif Nurrifky Wicaksono',
         'amount': 'amountTest',
-        'description': 'testing description'
+        'description': 'testing description',
+        'kelas' : 'PBP2'
     }
 
     return render(request, "main.html", context)
